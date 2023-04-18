16a8c7eac41fde28e9ffe226dccc819e6486ed05
Mochammad Hanif R <etrnal70@gmail.com>
2023-04-18 21:14:18 +0700

    push

--- keyboards/sofle/keymaps/etrnal70/config.h
+++ keyboards/sofle/keymaps/etrnal70/config.h
@@ -1,24 +1,26 @@
- /* Copyright 2023 Mochammad Hanif R <etrnal70>
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 2 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
+/* Copyright 2023 Mochammad Hanif R <etrnal70>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
 #define OLED_TIMEOUT 120000
-#define OLED_BRIGHTNESS 120
+#define OLED_BRIGHTNESS 80
 
+#undef TAPPING_TERM
+#define TAPPING_TERM 500
 #define SPLIT_WPM_ENABLE
 #define NO_MUSIC_MODE
 #define LAYER_STATE_8BIT
--- keyboards/sofle/keymaps/etrnal70/keymap.c
+++ keyboards/sofle/keymaps/etrnal70/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,      KC_U,       KC_I,    KC_O,    KC_P, KC_BSLS,
   CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,      KC_J,       KC_K,    KC_L, KC_SCLN, KC_QUOT,
   KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(2),      TO(1),     KC_N,      KC_M,    KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-                          KC_LCTL, MO(_SE), _______, KC_LALT, KC_ENT,    KC_SPC,  _______,   KC_RCTL,    MO(_SE),  KC_RCTL
+                          KC_LCTL, MO(_SE), _______, KC_LALT, KC_ENT,    KC_SPC,  KC_RALT,   _______,    MO(_SE),  KC_RCTL
 ),
 /* SECONDARY
  * ,-----------------------------------------.                    ,-----------------------------------------.
